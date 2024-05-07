a7c8370eade20bf188881e0fc498cad8573871dd
Mariah A. Knowles <snotskie@gmail.com>
2024-05-07 09:51:45 -0500

    fix yaml type

--- site/make.jl
+++ site/make.jl
@@ -1,14 +1,21 @@
 using Documenter
 using YAML
 
+# Helper to get the YAML return type coverted for makedocs correctly
+process(x) = x
+process(x::Dict) = collect(x)
+process(x::Vector) = process.(x)
+
+# Compile the website
 makedocs(
     sitename="DemoSite",
-    # authors="Mariah A. Knowles <snotskie@gmail.com> and contributors",
+    # authors="Mariah A. Knowles <snotskie@gmail.com> and contributors", # TODO
     format=Documenter.HTML(assets=[
         "assets/style.css",
         "assets/script.js"
     ]),
-    pages = YAML.load_file("site/toc.yaml")
+    pages = process(YAML.load_file("site/toc.yaml"))
 )
 
+# Deploy the website
 deploydocs(repo="github.com/Tiny-Earth/DemoSite.git")
